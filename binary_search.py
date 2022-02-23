cc40031173e9174ba23d4eb7167f292b8b21e1e2
钟善男 <897280922@qq.com>
2022-02-23 10:50:53 +0800

    2.23上午二分查找python

---
+++ binary_search.py
@@ -0,0 +1,17 @@
+def Binary_Search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (high + low) // 2
+
+        if item == list[mid]:
+            return mid
+        if item < list[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+List = [1,2,3,4,5,6,7,8]
+Item = 5
+print(Binary_Search(List,Item))
