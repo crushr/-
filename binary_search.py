0ba64b61c855fbe54294ed4b34f3971d3d8219d3
钟善男 <897280922@qq.com>
2022-02-24 20:27:53 +0800

    二分查找第二版

--- binary_search.py
+++ binary_search.py
@@ -1,22 +1,17 @@
-#二分查找
-def Binary_Search(list, item):
-    #定义查找的初始位和结束位，指向所查找的起始下标
-    low = 0
-    high = len(list) - 1
-    #当初始位和结束位之间还有值时，继续循环
-    while low <= high:
-        #定义中间位，向下取整
-        mid = (high + low) // 2
-        #先确定是否取到，再进行向左向右搜索，这很关键，如果先向左向右搜索会有bug
-        if item == list[mid]:
+# 二分查找2
+def binary_research(list, target):
+    head = 0
+    tail = len(list) - 1
+    while tail >= head:
+        mid = (head + tail) // 2
+        num = list[mid] #注意这边列表的应引用方式是中括号[]
+        if num == target:
             return mid
-        if item < list[mid]:
-            high = mid - 1
+        if  num > target:
+            tail = mid - 1
         else:
-            low = mid + 1
-    #无值返回none
+            head = mid +1
     return None
 
-List = [1,2,3,4,5,6,7,8]
-Item = 5
-print(Binary_Search(List,Item))
+List = [1,2,3,4,5,6]
+print(binary_research(List,6))
